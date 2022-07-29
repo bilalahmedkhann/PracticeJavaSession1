64598a7a4e2d54382380259c6627b90ce98eb002
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-07-29 13:08:57 +0530

    Demo Java program

---
+++ JavaPgm.java
@@ -0,0 +1,8 @@
+//What is the output for System.out.println((2+3)*4)
+
+public class JavaPgm {
+    public static void main(String[] args) {
+        System.out.println((2+3)*4);
+        System.out.println(9+6-3*4/2%5);
+    }
+}
