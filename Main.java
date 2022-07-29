9c3f135393628546b3e87903cbff62f823829df2
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-07-29 13:10:19 +0530

    Demo Java program

---
+++ Main.java
@@ -0,0 +1,8 @@
+//What is the output for System.out.println(-6+4*2);
+
+public class Main {
+    public static void main(String[] args) {
+        int a = (-6+4*2);
+        System.out.println(a);
+    }
+}
